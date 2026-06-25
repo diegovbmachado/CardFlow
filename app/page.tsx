328291814877d6e8b44d6dce6eefcb5c03573ac7
diego <diegovbmachado@gmail.com>
2026-06-25 18:42:45 -0300

    atualziando cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { DollarSign, Percent, Users } from "lucide-react";
 export default function Home() {
   return (
     <main className="sm:ml-14 -top-4">
-      <section  className="grid grid-cols-2 gap-4">
+      <section  className="grid grid-cols-2 lg:grid-cols-4 gap-4">
         <Card>
           <CardHeader>
             <div className="flex items-center justify-center">
