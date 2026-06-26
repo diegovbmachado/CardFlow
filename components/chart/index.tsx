287428c4e1b1c0b08f438061c624533dd51ea7ce
diego <diegovbmachado@gmail.com>
2026-06-26 17:31:53 -0300

    finalizando graficos

--- components/chart/index.tsx
+++ components/chart/index.tsx
@@ -1,18 +1,18 @@
 "use client"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DollarSign } from "lucide-react";
-import { ChartContainer, ChartConfig } from "../ui/chart";
+import { ChartContainer, ChartConfig, ChartTooltipContent, ChartTooltip } from "../ui/chart";
 import { Bar, CartesianGrid, XAxis, BarChart } from "recharts";
 
 export default function ChartOverview() {
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: "Janeiro", desktop: 186, mobile: 80 },
+  { month: "Fevereiro", desktop: 305, mobile: 200 },
+  { month: "Março", desktop: 237, mobile: 120 },
+  { month: "Abril", desktop: 73, mobile: 190 },
+  { month: "Maio", desktop: 209, mobile: 130 },
+  { month: "Junho", desktop: 214, mobile: 140 },
 ]
 const chartConfig = {
   desktop: {
@@ -26,7 +26,7 @@ const chartConfig = {
 } satisfies ChartConfig
 
   return (
-    <Card className="w-full md:w-1/2 md:max-w-600px">
+    <Card className="w-full md:w-1/3 md:max-w-600px">
       <CardHeader>
         <div className="flex items-center justify-center">
         <CardTitle className="text-lg sm:text-xl text-gray-800">
@@ -49,8 +49,9 @@ const chartConfig = {
         axisLine={false}
         tickFormatter={(value) => value.slice(0, 3)}
         />
+         <ChartTooltip content={<ChartTooltipContent />} />
         <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
         </BarChart>
           
         </ChartContainer>
