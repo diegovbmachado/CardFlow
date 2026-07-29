a97220758a28d05110e2edc06b766d2023b592f6
diego <diegovbmachado@gmail.com>
2026-07-28 23:25:17 -0300

    erro corrigido page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CryptoChart } from "@/components/dashboard/cryptochart/crypto-chart";
+import { CryptoChart } from "@/components/dashboard/cryptochart/page";
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
